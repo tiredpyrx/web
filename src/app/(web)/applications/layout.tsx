60de1b3cf9d13e7598f295036a0e866c1c59864f
Doga Korkmaz <dogakorkmaz09@gmail.com>
2024-06-13 18:37:52 +0300

    blog

--- src/app/(web)/applications/layout.tsx
+++ src/app/(web)/applications/layout.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 function ApplicationsLayout({children}: { children: React.ReactNode }): React.JSX.Element {
+    const range = "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     return (
         <div className="container mx-auto">
-            <div className={"grid grid-cols-4 gap-4"}>{children}</div>
+            <div className={"grid grid-cols-4 gap-4"}>
+                {Array.from([...(range.split(""))]).map(_ => children)}
+            </div>
         </div>
     );
 }
